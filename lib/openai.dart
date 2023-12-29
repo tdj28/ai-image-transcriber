55d9f16c530b97590a2a1836b8bdba5789c6ea1e
TJ <723947+tdj28@users.noreply.github.com>
2023-12-29 02:15:52 -0500

    chore: Update openai.dart
    Signed-off-by: TJ <723947+tdj28@users.noreply.github.com>

--- lib/openai.dart
+++ lib/openai.dart
@@ -2,24 +2,37 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<String> getResponseFromOpenAI(String base64Image) async {
+Future<String> getResponseFromOpenAI({String? base64Image, String? textInput}) async {
   try {
+    List<Map<String, dynamic>> contentList = [];
+
+    // Add text input to the content list if provided
+    if (textInput != null) {
+      contentList.add({"type": "text", "text": textInput});
+    }
+
+    // If an image is provided, add the specific prompt for the image
+    if (base64Image != null) {
+      // contentList.add({
+      //   "type": "text",
+      //   "text": "Here is an image, can you transcribe it? Please correct obvious spelling errors and grammar errors. Please only provide the transcript, unless you aren't sure about something in which case add some notes about that uncertainty and separate it from the transcript with ================================="
+      // });
+      contentList.add({"type": "image_url", "image_url": {"url": "data:image/jpeg;base64,$base64Image"}});
+    }
+
     var response = await http.post(
-      Uri.parse('https://api.openai.com/v1/chat/completions'), // Replace with the correct OpenAI endpoint
+      Uri.parse('https://api.openai.com/v1/chat/completions'),
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ${dotenv.env['OPENAI_KEY']}',
       },
       body: jsonEncode({
-        "model": "gpt-4-vision-preview", // Specify the model (if needed)
+        "model": "gpt-4-vision-preview",
         "max_tokens": 4096,
         "messages": [
           {
             "role": "user",
-            "content": [
-              {"type": "text", "text": "Here is an image, can you transcribe it? Please correct obvious spelling errors and grammar erros. Please only provide the transcript, unless you aren't sure about something in which case add some notes about that uncertainity and separate it from the transcript with ================================="},
-              {"type": "image_url", "image_url": {"url": "data:image/jpeg;base64,$base64Image"}}
-            ]
+            "content": contentList
           }
         ],
       }),
@@ -33,9 +46,8 @@ Future<String> getResponseFromOpenAI(String base64Image) async {
       print('Response body: ${response.body}');
       return "Sorry, I couldn't process that. Status Code: ${response.statusCode}";
     }
-  } catch (e, stackTrace) {
+  } catch (e) {
     print('An error occurred: $e');
-    print('Stack trace:\n$stackTrace');
     return "An error occurred: $e";
   }
 }
